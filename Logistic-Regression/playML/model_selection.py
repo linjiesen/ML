44266b0ab705c11b004df47f5f2e88b2b31dd0b4
Aurora <347625023@qq.com>
2019-03-05 21:38:58 +0800

    Add files via upload

---
+++ Logistic-Regression/playML/model_selection.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+
+def train_test_split(X, y, test_ratio=0.2, seed=None):
+    """Divide data X and y into X_train, X_test, y_train, y_test  according to test_ratio"""
+
+    assert X.shape[0] == y.shape[0],\
+        "the size of X must be equal to the size of y"
+    assert 0.0 <= test_ratio <= 1.0,\
+        "test_ratio must be valid"
+
+    if seed:
+        np.random.seed(seed)
+
+    shuffled_indexses = np.random.permutation(len(X))
+
+    test_size = int(len(X) * test_ratio)
+    test_indexes = shuffled_indexses[:test_size]
+    train_indexes = shuffled_indexses[test_size:]
+
+    X_train = X[train_indexes]
+    y_train = y[train_indexes]
+
+    X_test = X[test_indexes]
+    y_test = y[test_indexes]
+
+    return X_train, X_test, y_train, y_test
---
+++ myscript/hello.py
@@ -0,0 +1,5 @@
+#-*-coding:utf-8 -*-
+def hello(name):
+    print("Hello",name,"!")
+
+hello("Machine Learning")
